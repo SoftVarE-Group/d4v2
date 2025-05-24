448873f1042723300bb29584e813ff862f77b379
Jan Baudisch <jan.baudisch@uni-ulm.de>
2025-05-24 14:16:52 +0200

    feat(dependencies): update

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Packages and development environments for d4";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
   outputs =
     { self, nixpkgs, ... }:
